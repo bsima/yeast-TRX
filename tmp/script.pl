81cf71f3d40e3aaf4d27f26bec64a90d2a843dcf
bsima <bensima@gmail.com>
2013-07-08 12:40:59 -0400

    Submodule reverseCompliment was drafted, will need more work

--- tmp/script.pl
+++ tmp/script.pl
@@ -15,10 +15,34 @@ foreach my $fileName (@dirFiles) {
 		open(FILE, $fileName) || die "Cannot open file: $!";
 		
 		while ( my $line = <FILE> ) {
-				
-				if ( $line =~ m/^Scer\s{12}([atgcATGC]{1,})/ ) {
+			
+				# If it's a Crick file, reverse it
+				if ( $fileName =~ /.{1,}(C.aln)/ ) {
+						$line = reverseCompliment($line);
+						print "Line reversed\n";
+						print $line . "\n";
+				};
+
+				# Print the matched line
+				if ( $line =~ /^Scer\s{12}([atgcATGC]{1,})/ ) {
 						print $1 . "\n";
 				};
 		};
 		close FILE;
 };
+
+
+sub reverseCompliment {
+
+		# Get DNA to work on
+		my ($dna) = @_;
+
+		# First reverse the DNA strand
+		$dna = reverse $dna;
+
+		# Now translate the DNA
+		$dna =~ tr/ACGTacgt/TGCAtgca/;
+
+		# Return the output
+		return $dna;
+}

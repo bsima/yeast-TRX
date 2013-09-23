c0066a76efd38d111b13c82df60110a46d4dc39d
bsima <bensima@gmail.com>
2013-09-23 04:21:56 -0400

    Simplified the R script, but there are some bugs

--- plot.R
+++ plot.R
@@ -1,27 +1,51 @@
-# setwd("~/Code/Babbit")
-bayanusTRX <- read.table("bayanus-TRXscore.csv",sep=",",header=TRUE)
-cerevisiaeTRX <- read.table("cerevisiae-TRXscore.csv",sep=",",header=TRUE)
-martinaeTRX <- read.table("martinae-TRXscore.csv",sep=",",header=TRUE)
-paradoxusTRX <- read.table("paradoxus-TRXscore.csv",sep=",",header=TRUE)
-
 library(ggplot2)
-bayanusPlot<-qplot(data = bayanusTRX, x=position, y=trx.score, facets=~gene, color=gene)
-bayanusPlot
-ggsave("bayanusPlot.png")
 
-cerevisiaePlot<-qplot(data = cerevisiaeTRX, x=position, y=trx.score, facets=~gene, color=gene)
-cerevisiaePlot
-ggsave("cerevisiaePlot.png")
+makePlot <- function(species) {
+    x = "read.table('./data/"
+    y = "-TRXscore.csv',sep=',',header=TRUE)" 
+    species.TRX = paste(x, species, y, sep="") 
+    
+    # species.TRX = paste("read.table('./data/","species",,sep="")
+    # species.TRX <- read.table("./data/"species"-TRXscore.csv",sep=",",header=TRUE)
+    species.Plot <- qplot(data = species.TRX, x=position, y=trx.score, facets=~gene, color=gene)
+    species.Plot
+    
+    a = "'plot/"
+    b = "Plot.png'"
+    plotPath = paste(a, species, b, sep="")
+    ggsave(plotPath)
+}
 
-martinaePlot<-qplot(data = martinaeTRX, x=position, y=trx.score, facets=~gene, color=gene)
-martinaePlot
-ggsave("martinaePlot.png")
+# setwd("~/Code/Babbit")
+# bayanusTRX <- read.table("./data/bayanus-TRXscore.csv",sep=",",header=TRUE)
+# cerevisiaeTRX <- read.table("./data/cerevisiae-TRXscore.csv",sep=",",header=TRUE)
+# martinaeTRX <- read.table("./data/martinae-TRXscore.csv",sep=",",header=TRUE)
+# paradoxusTRX <- read.table("./data/paradoxus-TRXscore.csv",sep=",",header=TRUE)
 
-paradoxusPlot<-qplot(data = paradoxusTRX, x=position, y=trx.score, facets=~gene, color=gene)
-paradoxusPlot
-ggsave("paradoxusPlot.png")
+speciesNames = c("bayanus","cerevisiae","martinae","paradoxus")
+
+for (species in speciesNames) {
+    makePlot(species)
+}
 
 
+# bayanusPlot<-qplot(data = bayanusTRX, x=position, y=trx.score, facets=~gene, color=gene)
+# bayanusPlot
+# ggsave("plot/bayanusPlot.png")
+# 
+# cerevisiaePlot<-qplot(data = cerevisiaeTRX, x=position, y=trx.score, facets=~gene, color=gene)
+# cerevisiaePlot
+# ggsave("plot/cerevisiaePlot.png")
+# 
+# martinaePlot<-qplot(data = martinaeTRX, x=position, y=trx.score, facets=~gene, color=gene)
+# martinaePlot
+# ggsave("plot/martinaePlot.png")
+# 
+# paradoxusPlot<-qplot(data = paradoxusTRX, x=position, y=trx.score, facets=~gene, color=gene)
+# paradoxusPlot
+# ggsave("plot/paradoxusPlot.png")
+
+q()
 
 # rm(bayanusTRX)
 # rm(cerevisiaeTRX)

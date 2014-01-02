3e64372fcc48faf175ce444399c0e34a8e78e464
bsima <bensima@gmail.com>
2014-01-02 17:51:12 -0500

    Cleaned up the R code a bit. Now need to finish the weighted correlation calculation

--- code/plot.R
+++ code/plot.R
@@ -1,50 +1,59 @@
 library(plyr)
 library(ggplot2)
 
-geneNameDirs = './data/YeastGenome-tmp' 
-geneNames    = list.files(path = geneNameDirs, pattern = '(e.{1,})[[:punct:]]aln')
-geneNames    = sub('[[:punct:]]aln', '', geneNames) # This gets rid of the filename .aln ending
+# Some ideas for sensible coding:
+#
+# * Prefix my variable names with something, such as `my` or `var`. This doesn't actually scope them, it just signifies that I set them.
+# * Setup the root directory var again.
+# * Open this script in RStudio for testing.
+# * I think this will become the `server.ui` logic of the Shiny app. It might be useful to check that out, and finish writing
+#   the script with the Shiny app in mind.
 
-plotMe = function(species) {
+myPath = getcwd()
+myRoot = sub('code$', '', myPath)
 
-    speciesFolder = paste('./plots/', species, sep = '')
-    dir.create(speciesFolder)
 
-    for (gene in geneNames) {
+myGeneNameDirs = paste(myRoot, '/data/YeastGenome-tmp', sep = '')
+myGeneNames    = list.files(path = myGeneNameDirs, pattern = '(e.{1,})[[:punct:]]aln')
+myGeneNames    = sub('[[:punct:]]aln', '', myGeneNames) # This gets rid of the filename .aln ending
 
-        # geneFolder = paste('./plots/', species, '/', gene, sep = '')
-        # dir.create(geneFolder)
+myMakePlot = function(species) {
 
-        smoothData = paste('./data/', species, '/', gene, '/smooth.csv', sep = '') 
-    
-        df = read.table(smoothData, sep = ',', header = TRUE)
+    mySpeciesFolder = paste(myRoot, '/plots/', species, sep = '')
+    dir.create(mySpeciesFolder)
+
+    for (myGene in myGeneNames) {
+
+        mySmoothData = paste(myRoot, '/data/', species, '/', gene, '/smooth.csv', sep = '')
+
+        myData = read.table(mySmoothData, sep = ',', header = TRUE)
 
         # plot = qplot(data = df, x = 1:length(df$position), y = trxMean, facets =~ gene, color = gene) + geom_line()
         #genePosition = df$position
         #trxMean = df$trxMean
         #energyMean = df$energyMean
-        correlation = cor(x = df$trxMean, y = abs(df$energyMean), use = "na.or.complete")
+        myCorrelation = cor(x = myData$trxMean, y = abs(myData$energyMean), use = "na.or.complete")
 
-        WC( df$postion, df$  )
+        WC( myData$postion, myData$  )
 
-        plot = ggplot() +
-                   geom_point(data = df, aes(x = position, y = trxMean), color = 'blue') +
-                   geom_point(data = df, aes(x = position, y = abs(energyMean)), color = 'red') +
-                   geom_line(data = df, aes(x = position, y = trxMean)) +
-                   geom_line(data = df, aes(x = position, y = abs(energyMean))) +
+        myPlot = ggplot() +
+                   geom_point(data = myData, aes(x = position, y = trxMean), color = 'blue') +
+                   geom_point(data = myData, aes(x = position, y = abs(energyMean)), color = 'red') +
+                   geom_line(data = myData, aes(x = position, y = trxMean)) +
+                   geom_line(data = myData, aes(x = position, y = abs(energyMean))) +
                    annotate("text", label = paste('r=', correlation, sep=''),x=1000,y=5,size=5, color = 'red')
-        plot
+        myPlot # Generates the plot
 
-        plotPath = paste('./plots/', species, '/', gene, '.png', sep = '')
+        myPlotPath = paste('./plots/', species, '/', gene, '.png', sep = '')
         ggsave(plotPath)
     }
 }
 
-speciesNames = c("bayanus","cerevisiae","martinae","paradoxus")
+mySpeciesNames = c("bayanus","cerevisiae","martinae","paradoxus")
 #geneNames    = c("eYAL001C","eYCL031C","eYDR181C","eYER111C","eYGR100W")
 
-for (x in speciesNames) {    
-    plotMe(x)
+for (x in mySpeciesNames) {
+    myMakePlot(x)
 }
 
 #q()
